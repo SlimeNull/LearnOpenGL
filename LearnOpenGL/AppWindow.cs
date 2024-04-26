cd3ee5cbb38251fc51effb8c5891ca6e23fc7ad5
SlimeNull <slimenull@outlook.com>
2024-04-26 16:42:58 +0800

    cube!

--- LearnOpenGL/AppWindow.cs
+++ LearnOpenGL/AppWindow.cs
@@ -1,9 +1,10 @@
 ﻿using System.ComponentModel;
 using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
-using StbImageSharp;
+using StbiSharp;
 
 namespace LearnOpenGL;
 
@@ -16,10 +17,63 @@ partial class AppWindow : GameWindow
     // OpenGL only supports rendering in 3D, so to create a flat triangle, the Z coordinate will be kept as 0.
     private readonly float[] _vertices =
     {
-        -0.5f, -0.5f, 0.0f, 0, 1, // Bottom-left vertex
-        0.5f,  -0.5f, 0.0f, 1, 1, // Bottom-right vertex
-        0.0f,  0.5f,  0.0f, 0.5f, 0  // Top vertex
+        // front
+        -0.5f, -0.5f, 0.5f, 0, 0,
+        -0.5f, 0.5f, 0.5f, 0, 1,
+        0.5f, 0.5f, 0.5f, 1, 1,
+
+        -0.5f, -0.5f, 0.5f, 0, 0,
+        0.5f, 0.5f, 0.5f, 1, 1,
+        0.5f, -0.5f, 0.5f, 1, 0,
+
+        // top
+        -0.5f, 0.5f, 0.5f, 0, 0,
+        -0.5f, 0.5f, -0.5f, 0, 1,
+        0.5f, 0.5f, -0.5f, 1, 1,
+
+        -0.5f, 0.5f, 0.5f, 0, 0,
+        0.5f, 0.5f, -0.5f, 1, 1,
+        0.5f, 0.5f, 0.5f, 1, 0,
+        
+        // bottom
+        -0.5f, -0.5f, -0.5f, 0, 0,
+        -0.5f, -0.5f, 0.5f, 0, 1,
+        0.5f, -0.5f, 0.5f, 1, 1,
+
+        -0.5f, -0.5f, -0.5f, 0, 0,
+        0.5f, -0.5f, 0.5f, 1, 1,
+        0.5f, -0.5f, -0.5f, 1, 0,
+
+        // right
+        0.5f, -0.5f, 0.5f, 0, 0,
+        0.5f, 0.5f, 0.5f, 0, 1,
+        0.5f, 0.5f, -0.5f, 1, 1,
+
+        0.5f, -0.5f, 0.5f, 0, 0,
+        0.5f, 0.5f, -0.5f, 1, 1,
+        0.5f, -0.5f, -0.5f, 1, 0,
+
+        // left
+        -0.5f, -0.5f, -0.5f, 0, 0,
+        -0.5f, 0.5f, -0.5f, 0, 1,
+        -0.5f, 0.5f, 0.5f, 1, 1,
+
+        -0.5f, -0.5f, -0.5f, 0, 0,
+        -0.5f, 0.5f, 0.5f, 1, 1,
+        -0.5f, -0.5f, 0.5f, 1, 0,
+
+        // back
+        0.5f, -0.5f, -0.5f, 0, 0,
+        0.5f, 0.5f, -0.5f, 0, 1,
+        -0.5f, 0.5f, -0.5f, 1, 1,
+
+        0.5f, -0.5f, -0.5f, 0, 0,
+        -0.5f, 0.5f, -0.5f, 1, 1,
+        -0.5f, -0.5f, -0.5f, 1, 0,
     };
+
+    private float _moveSpeed = 1;
+    private float _radianPerPixel = MathF.PI / 100;
 
     // These are the handles to OpenGL objects. A handle is an integer representing where the object lives on the
     // graphics card. Consider them sort of like a pointer; we can't do anything with them directly, but we can
@@ -27,21 +81,63 @@ partial class AppWindow : GameWindow
 
     // What these objects are will be explained in OnLoad.
     private int _vertexBufferObject;
-
     private int _vertexArrayObject;
 
     // This class is a wrapper around a shader, which helps us manage it.
     // The shader class's code is in the Common project.
     // What shaders are and what they're used for will be explained later in this tutorial.
-    private Shader _shader;
+    private Shader? _shader;
+    private Matrix4 _translateMatrix4 = Matrix4.Identity;
+    private Matrix4 _rotationMatrix4 = Matrix4.Identity;
+    private Matrix4 _scaleMatrix4 = Matrix4.Identity;
+
+
 
     public AppWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
         : base(gameWindowSettings, nativeWindowSettings)
     {
     }
 
+    private Matrix4 GetRotationMatrix(float xRadian, float yRadian, float zRadian)
+    {
+        Matrix4 result = new Matrix4(
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1);
+
+        if (xRadian != 0)
+        {
+            result = new Matrix4(
+                1, 0, 0, 0,
+                0, MathF.Cos(xRadian), -MathF.Sin(xRadian), 0,
+                0, MathF.Sin(xRadian), MathF.Cos(xRadian), 0,
+                0, 0, 0, 1) * result;
+        }
+
+        if (yRadian != 0)
+        {
+            result = new Matrix4(
+                MathF.Cos(yRadian), 0, MathF.Sin(yRadian), 0,
+                0, 1, 0, 0,
+                -MathF.Sin(yRadian), 0, MathF.Cos(yRadian), 0,
+                0, 0, 0, 1) * result;
+        }
+
+        if (zRadian != 0)
+        {
+            result = new Matrix4(
+                MathF.Cos(zRadian), -MathF.Sin(zRadian), 0, 0,
+                MathF.Sin(zRadian), MathF.Cos(zRadian), 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1) * result;
+        }
+
+        return result;
+    }
+
     // Now, we start initializing OpenGL.
-    protected override void OnLoad()
+    protected override unsafe void OnLoad()
     {
         base.OnLoad();
 
@@ -51,92 +147,59 @@ partial class AppWindow : GameWindow
         // This is a deep green.
         GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-        // We need to send our vertices over to the graphics card so OpenGL can use them.
-        // To do this, we need to create what's called a Vertex Buffer Object (VBO).
-        // These allow you to upload a bunch of data to a buffer, and send the buffer to the graphics card.
-        // This effectively sends all the vertices at the same time.
-
-        // First, we need to create a buffer. This function returns a handle to it, but as of right now, it's empty.
         _vertexBufferObject = GL.GenBuffer();
-
-        // Now, bind the buffer. OpenGL uses one global state, so after calling this,
-        // all future calls that modify the VBO will be applied to this buffer until another buffer is bound instead.
-        // The first argument is an enum, specifying what type of buffer we're binding. A VBO is an ArrayBuffer.
-        // There are multiple types of buffers, but for now, only the VBO is necessary.
-        // The second argument is the handle to our buffer.
         GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
 
-        // Finally, upload the vertices to the buffer.
-        // Arguments:
-        //   Which buffer the data should be sent to.
-        //   How much data is being sent, in bytes. You can generally set this to the length of your array, multiplied by sizeof(array type).
-        //   The vertices themselves.
-        //   How the buffer will be used, so that OpenGL can write the data to the proper memory space on the GPU.
-        //   There are three different BufferUsageHints for drawing:
-        //     StaticDraw: This buffer will rarely, if ever, update after being initially uploaded.
-        //     DynamicDraw: This buffer will change frequently after being initially uploaded.
-        //     StreamDraw: This buffer will change on every frame.
-        //   Writing to the proper memory space is important! Generally, you'll only want StaticDraw,
-        //   but be sure to use the right one for your use case.
+
         GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
 
-        // One notable thing about the buffer we just loaded data into is that it doesn't have any structure to it. It's just a bunch of floats (which are actaully just bytes).
-        // The opengl driver doesn't know how this data should be interpreted or how it should be divided up into vertices. To do this opengl introduces the idea of a 
-        // Vertex Array Obejct (VAO) which has the job of keeping track of what parts or what buffers correspond to what data. In this example we want to set our VAO up so that 
-        // it tells opengl that we want to interpret 12 bytes as 3 floats and divide the buffer into vertices using that.
-        // To do this we generate and bind a VAO (which looks deceptivly similar to creating and binding a VBO, but they are different!).
+
         _vertexArrayObject = GL.GenVertexArray();
         GL.BindVertexArray(_vertexArrayObject);
 
-        // Now, we need to setup how the vertex shader will interpret the VBO data; you can send almost any C datatype (and a few non-C ones too) to it.
-        // While this makes them incredibly flexible, it means we have to specify how that data will be mapped to the shader's input variables.
 
-        // To do this, we use the GL.VertexAttribPointer function
-        // This function has two jobs, to tell opengl about the format of the data, but also to associate the current array buffer with the VAO.
-        // This means that after this call, we have setup this attribute to source data from the current array buffer and interpret it in the way we specified.
-        // Arguments:
-        //   Location of the input variable in the shader. the layout(location = 0) line in the vertex shader explicitly sets it to 0.
-        //   How many elements will be sent to the variable. In this case, 3 floats for every vertex.
-        //   The data type of the elements set, in this case float.
-        //   Whether or not the data should be converted to normalized device coordinates. In this case, false, because that's already done.
-        //   The stride; this is how many bytes are between the last element of one vertex and the first element of the next. 3 * sizeof(float) in this case.
-        //   The offset; this is how many bytes it should skip to find the first element of the first vertex. 0 as of right now.
-        // Stride and Offset are just sort of glossed over for now, but when we get into texture coordinates they'll be shown in better detail.
         GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
         GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
 
-        // Enable variable 0 in the shader.
+
         GL.EnableVertexAttribArray(0);
         GL.EnableVertexAttribArray(1);
 
-        // We've got the vertices done, but how exactly should this be converted to pixels for the final image?
-        // Modern OpenGL makes this pipeline very free, giving us a lot of freedom on how vertices are turned to pixels.
-        // The drawback is that we actually need two more programs for this! These are called "shaders".
-        // Shaders are tiny programs that live on the GPU. OpenGL uses them to handle the vertex-to-pixel pipeline.
-        // Check out the Shader class in Common to see how we create our shaders, as well as a more in-depth explanation of how shaders work.
+
         // shader.vert and shader.frag contain the actual shader code.
         using var vertShaderSource = File.OpenText("Shaders/shader.vert");
         using var fragShaderSource = File.OpenText("Shaders/shader.frag");
         _shader = new Shader(vertShaderSource, fragShaderSource);
 
-        var avatarStream = File.OpenRead("Assets/avatar.jpg");
-        var image = ImageResult.FromStream(avatarStream);
+        using var avatarFileStream = File.OpenRead("Assets/avatar.jpg");
+        using var avatarMemoryStream = new MemoryStream((int)avatarFileStream.Length);
+        avatarFileStream.CopyTo(avatarMemoryStream);
 
-        GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, image.Width, image.Height, 0, PixelFormat.Rgb, PixelType.UnsignedByte, image.Data);
-        GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+        var image = Stbi.LoadFromMemory(avatarMemoryStream, 3);
+
+        var texture = GL.GenTexture();
+        GL.BindTexture(TextureTarget.Texture2D, texture);
+        GL.ActiveTexture(TextureUnit.Texture0);
+
+        fixed (byte* imageData = image.Data)
+        {
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, image.Width, image.Height, 0, PixelFormat.Rgb, PixelType.UnsignedByte, (nint)imageData);
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+        }
 
         //var texture = GL.GenTexture();
         //GL.BindTexture(TextureTarget.Texture2D, texture);
         //GL.GenerateTextureMipmap(texture);
 
-        // Now, enable the shader.
-        // Just like the VBO, this is global, so every function that uses a shader will modify this one until a new one is bound instead.
+
         _shader.Use();
 
-        // Setup is now complete! Now we move to the OnRenderFrame function to finally draw the triangle.
+        GL.Enable(EnableCap.CullFace);
+        GL.FrontFace(FrontFaceDirection.Cw);
     }
 
     // Now that initialization is done, let's create our render loop.
+
     protected override void OnRenderFrame(FrameEventArgs e)
     {
         base.OnRenderFrame(e);
@@ -153,7 +216,13 @@ partial class AppWindow : GameWindow
         // and then calling an OpenGL function to render.
 
         // Bind the shader
-        _shader.Use();
+        _shader!.Use();
+
+        var transformLocation = GL.GetUniformLocation(_shader.Handle, "transform");
+        var finalMatrix = _translateMatrix4 * _rotationMatrix4 * _scaleMatrix4;
+        GL.UniformMatrix4(transformLocation, false, ref finalMatrix);
+
+
 
         // Bind the VAO
         GL.BindVertexArray(_vertexArrayObject);
@@ -166,7 +235,8 @@ partial class AppWindow : GameWindow
         //     is some variant of a triangle. Since we just want a single triangle, we use Triangles.
         //   Starting index; this is just the start of the data you want to draw. 0 here.
         //   How many vertices you want to draw. 3 for a triangle.
-        GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        GL.DrawArrays(PrimitiveType.Triangles, 0, 3 * 12);
+
 
         // OpenTK windows are what's known as "double-buffered". In essence, the window manages two buffers.
         // One is rendered to while the other is currently displayed by the window.
@@ -181,11 +251,88 @@ partial class AppWindow : GameWindow
     {
         base.OnUpdateFrame(e);
 
-        var input = KeyboardState;
+        var keyboardInput = KeyboardState;
+        var mouseInput = MouseState;
 
-        if (input.IsKeyDown(Keys.Escape))
+        if (keyboardInput.IsKeyDown(Keys.Escape))
         {
             Close();
+        }
+
+        if (keyboardInput.IsKeyDown(Keys.Space))
+        {
+            _translateMatrix4.Column3 = _translateMatrix4.Column3 with
+            {
+                Y = (float)(_translateMatrix4.Column3.Y + _moveSpeed * e.Time)
+            };
+        }
+
+        if (keyboardInput.IsKeyDown(Keys.LeftShift))
+        {
+            _translateMatrix4.Column3 = _translateMatrix4.Column3 with
+            {
+                Y = (float)(_translateMatrix4.Column3.Y - _moveSpeed * e.Time)
+            };
+        }
+
+        if (keyboardInput.IsKeyDown(Keys.W))
+        {
+            _translateMatrix4.Column3 = _translateMatrix4.Column3 with
+            {
+                Z = (float)(_translateMatrix4.Column3.Z - _moveSpeed * e.Time)
+            };
+        }
+
+        if (keyboardInput.IsKeyDown(Keys.W))
+        {
+            _translateMatrix4.Column3 = _translateMatrix4.Column3 with
+            {
+                Z = (float)(_translateMatrix4.Column3.Z + _moveSpeed * e.Time)
+            };
+        }
+
+        if (keyboardInput.IsKeyDown(Keys.A))
+        {
+            _translateMatrix4.Column3 = _translateMatrix4.Column3 with
+            {
+                X = (float)(_translateMatrix4.Column3.X - _moveSpeed * e.Time)
+            };
+        }
+
+        if (keyboardInput.IsKeyDown(Keys.D))
+        {
+            _translateMatrix4.Column3 = _translateMatrix4.Column3 with
+            {
+                X = (float)(_translateMatrix4.Column3.X + _moveSpeed * e.Time)
+            };
+        }
+
+        if (mouseInput.IsButtonDown(MouseButton.Left))
+        {
+            var xOffset = mouseInput.X - mouseInput.PreviousX;
+            var yOffset = mouseInput.Y - mouseInput.PreviousY;
+
+            _rotationMatrix4 = GetRotationMatrix(_radianPerPixel * yOffset, _radianPerPixel * xOffset, 0) * _rotationMatrix4;
+        }
+
+        if (mouseInput.ScrollDelta != default)
+        {
+            if (mouseInput.ScrollDelta.Y > 0)
+            {
+                _scaleMatrix4 *= new Matrix4(
+                    1.1f, 0, 0, 0,
+                    0, 1.1f, 0, 0,
+                    0, 0, 1.1f, 0,
+                    0, 0, 0, 1);
+            }
+            else if (mouseInput.ScrollDelta.Y < 0)
+            {
+                _scaleMatrix4 *= new Matrix4(
+                    0.9f, 0, 0, 0,
+                    0, 0.9f, 0, 0,
+                    0, 0, 0.9f, 0,
+                    0, 0, 0, 1);
+            }
         }
     }
 
@@ -222,7 +369,10 @@ partial class AppWindow : GameWindow
         GL.DeleteBuffer(_vertexBufferObject);
         GL.DeleteVertexArray(_vertexArrayObject);
 
-        GL.DeleteProgram(_shader.Handle);
+        if (_shader is not null)
+        {
+            GL.DeleteProgram(_shader.Handle);
+        }
 
         base.OnUnload();
     }
--- LearnOpenGL/Shaders/shader.vert
+++ LearnOpenGL/Shaders/shader.vert
@@ -1,10 +1,11 @@
 ﻿#version 330 core
 layout (location = 0) in vec3 position0;
 layout (location = 1) in vec2 uv0;
+uniform mat4 transform;
 out vec2 uvOutput0;
 
 void main()
 {
-    gl_Position = vec4(position0, 1.0);
+    gl_Position = vec4(position0, 1.0) * transform;
     uvOutput0 = uv0;
 }
