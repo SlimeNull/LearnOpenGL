d431747b25f930cdeb384842f83ef5baa0464d9e
SlimeNull <slimenull@outlook.com>
2024-04-26 09:28:20 +0800

    添加项目文件。

---
+++ LearnOpenGL/Program.cs
@@ -0,0 +1,14 @@
+﻿using LearnOpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+
+var nativeWindowSettings = new NativeWindowSettings()
+{
+    ClientSize = new Vector2i(800, 600),
+    Title = "Learn OpenGL"
+};
+
+using (var window = new AppWindow(GameWindowSettings.Default, nativeWindowSettings))
+{
+    window.Run();
+}
